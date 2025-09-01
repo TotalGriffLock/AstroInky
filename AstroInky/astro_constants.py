d77357795683fbea62395fb6655f45551538633e
John Griffith <john@theshado.ws>
2025-09-01 20:47:31 +0100

    Added files missing in previous commit

---
+++ AstroInky/astro_constants.py
@@ -0,0 +1,86 @@
+from inky.eeprom import read_eeprom
+from PIL import Image,ImageFont
+
+# Colour stuff
+# There are 2 versions of the 7.3" Inky Impression
+# Work out which one we have and what colours it supports
+display = read_eeprom()
+if display is None:
+  print("Could not detect your Inky Impression")
+  exit()
+if display.get_color() == "spectra6":
+  INKY_COLORS = {
+      "BLACK":  (0, 0, 0),
+      "WHITE":  (255, 255, 255),
+      "YELLOW":    (255, 255, 0),
+      "RED":  (255, 0, 0),
+      "BLUE":   (0, 0, 255),
+      "GREEN": (0, 255, 0)
+  }
+  BLACK  = INKY_COLORS["BLACK"]
+  WHITE  = INKY_COLORS["WHITE"]
+  RED    = INKY_COLORS["RED"]
+  GREEN  = INKY_COLORS["GREEN"]
+  BLUE   = INKY_COLORS["BLUE"]
+  YELLOW = INKY_COLORS["YELLOW"]
+  color_palette = [
+      0, 0, 0,         # 0: Black
+      255, 255, 255,   # 1: White
+      255, 0, 0,       # 2: Red
+      0, 255, 0,       # 3: Green
+      0, 0, 255,       # 4: Blue
+      255, 255, 0,     # 5: Yellow
+  ] + [0, 0, 0]        # Fill the rest with black
+elif display.get_color() == "7colour":
+  INKY_COLORS = {
+      "BLACK":  (0, 0, 0),
+      "WHITE":  (255, 255, 255),
+      "YELLOW":    (255, 255, 0),
+      "RED":  (255, 0, 0),
+      "BLUE":   (0, 0, 255),
+      "GREEN": (0, 255, 0),
+      "ORANGE": (255, 128, 0)
+  }
+  BLACK  = INKY_COLORS["BLACK"]
+  WHITE  = INKY_COLORS["WHITE"]
+  RED    = INKY_COLORS["RED"]
+  GREEN  = INKY_COLORS["GREEN"]
+  BLUE   = INKY_COLORS["BLUE"]
+  YELLOW = INKY_COLORS["YELLOW"]
+  ORANGE = INKY_COLORS["ORANGE"]
+  color_palette = [
+      0, 0, 0,         # 0: Black
+      255, 255, 255,   # 1: White
+      255, 0, 0,       # 2: Red
+      0, 255, 0,       # 3: Green
+      0, 0, 255,       # 4: Blue
+      255, 255, 0,     # 5: Yellow
+      255, 128, 0      # 6: Orange
+  ] + [0, 0, 0]        # Fill the rest with black
+else:
+  print("Could not detect what colour palette your Inky Impression supports")
+  exit()
+
+bw_palette = [
+    0, 0, 0,         # Black
+    255, 255, 255    # White
+] + [0, 0, 0]
+bw_palette_img = Image.new("P", (16, 16))
+bw_palette_img.putpalette(bw_palette)
+color_palette_img = Image.new("P", (16, 16))
+color_palette_img.putpalette(color_palette)
+#endregion
+#region TEXT, FONT & ICON SETTINGS
+#FONTS
+font = ImageFont.truetype("fonts/Merriweather-VariableFont_opsz,wdth,wght.ttf", 18)
+font_path = "fonts/Merriweather-VariableFont_opsz,wdth,wght.ttf"
+font_large = ImageFont.truetype("fonts/Merriweather-VariableFont_opsz,wdth,wght.ttf", 24)
+ascent, descent = font.getmetrics()                                                 #gets the height (ascent and descent) of the font
+font_height = ascent + descent
+line_spacing = font_height * 2
+#ICONS
+icon_size_lg = (font_height * 2, font_height * 2)
+icon_size_sm = (font_height, font_height)
+icon_size_xl = (font_height * 10, font_height * 10)
+#TEXTWRAPPING
+nav_y = 10
---
+++ AstroInky/astro_functions.py
@@ -0,0 +1,165 @@
+import astro_constants
+import cairosvg
+import io
+import re
+import configparser
+import time
+
+from zoneinfo import ZoneInfo
+from timezonefinder import timezone_at
+from PIL import Image, ImageDraw
+from inky.auto import auto
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Init display
+def init_display():
+  inky_display = auto()
+  display_height, display_width = inky_display.resolution                             #defines a canvas
+  base_img = Image.new("RGB", (display_width, display_height), (255, 255, 255))           #creates the initial background object (img) - mode, size, color
+  draw = ImageDraw.Draw(base_img)
+  nav_about = load_svg_icon("icons/refresh.svg", size=astro_constants.icon_size_sm)
+  nav_solar = load_svg_icon("icons/solar.svg", size=astro_constants.icon_size_sm)
+  nav_star = load_svg_icon("icons/planet.svg", size=astro_constants.icon_size_sm)
+  nav_phase = load_svg_icon("icons/moon.svg", size=astro_constants.icon_size_sm)
+  nav_y = 10
+  xA = ((display_width // 4) // 2) - (astro_constants.icon_size_sm[0] // 2) -1
+  xB = ((display_width // 4) // 2) * 3 - (astro_constants.icon_size_sm[0] // 2) -1
+  xC = ((display_width // 4) // 2) * 5 - (astro_constants.icon_size_sm[0] // 2) -1
+  xD = ((display_width // 4) // 2) * 7 - (astro_constants.icon_size_sm[0] // 2) -1
+  base_img.paste(nav_phase, (xA, nav_y))
+  base_img.paste(nav_star, (xB, nav_y))
+  base_img.paste(nav_solar, (xC, nav_y))
+  base_img.paste(nav_about, (xD, nav_y))
+  return (inky_display, base_img, display_height, display_width, draw)
+
+
+def wrap_text(text, font, draw, max_width, max_lines):
+    words = text.split()
+    lines = []
+    line = ""
+
+    for word in words:
+        test_line = line + word + " "
+        if draw.textlength(test_line, font=font) <= max_width:
+            line = test_line
+        else:
+            lines.append(line.rstrip())
+            line = word + " "
+        if len(lines) >= max_lines:
+            break
+
+    if line and len(lines) < max_lines:
+        lines.append(line.rstrip())
+    elif len(lines) == max_lines:
+        # Join all visible lines so far
+        full_text = " ".join(lines) + " " + line
+
+        # Find the last full sentence ending
+        match = list(re.finditer(r'([.!?])\s+', full_text))
+        if match:
+            last_end = match[-1].end()
+            clean_text = full_text[:last_end].strip()
+        else:
+            clean_text = full_text.strip()
+
+        # Re-wrap from clean_text
+        lines = []
+        line = ""
+        for word in clean_text.split():
+            test_line = line + word + " "
+            if draw.textlength(test_line, font=font) <= max_width:
+                line = test_line
+            else:
+                lines.append(line.rstrip())
+                line = word + " "
+                if len(lines) >= max_lines - 1:
+                    break
+        if line:
+            lines.append(line.rstrip())
+
+        # Append ellipsis if the original text was longer
+        if not full_text.strip().endswith(clean_text.strip()):
+            lines[-1] = lines[-1].rstrip(".!? ") + "..."
+
+    return lines
+
+# endregion
+#region SVG Handler
+
+def load_svg_icon(svg_path, size=(24, 24)):
+    # Render SVG to PNG in memory
+    png_data = cairosvg.svg2png(url=svg_path, output_width=size[0], output_height=size[1])
+    icon_rgba = Image.open(io.BytesIO(png_data)).convert("RGBA")
+    icon_bg = Image.new("RGBA", size, (255, 255, 255, 255))
+    icon_flat = Image.alpha_composite(icon_bg, icon_rgba)
+    icon_final = icon_flat.convert("RGB").quantize(palette=astro_constants.color_palette_img)
+    icon = Image.new("RGBA", icon_rgba.size)
+    icon.paste(icon_final)
+    return icon
+#endregion
+
+# Display finished output
+def astro_display(base_img, inky_display):
+    final_img = base_img.rotate(90, expand=True)
+    final_img = final_img.quantize(palette=astro_constants.color_palette_img)
+    inky_display.set_image(final_img)
+    inky_display.show()
+
+# Handle location info
+def get_location():
+  # To GPS or not to GPS
+  yes_words = ('y', 'Y','yes','Yes','YES',1,'true','True',True)
+
+  if config['location']['use_gps'] in yes_words:
+    from pa1010d import PA1010D
+    gps = PA1010D()
+    try:
+      result = gps.update()
+    except OSError as error:
+      # GPS wasn't found
+      # Initialise display
+      inky_display, base_img, display_height, display_width, draw = init_display()
+      align_center = display_width // 2
+      message_top = "GPS enabled in config.ini but device not found"
+      top_center = align_center - (draw.textlength(message_top, font=astro_constants.font) / 2)
+      draw.text((top_center, 100), message_top, astro_constants.RED, font=astro_constants.font)
+      draw.text((top_center, 200), str(error), astro_constants.RED, font=astro_constants.font)
+
+      # Error Display
+      astro_display(base_img, inky_display)
+      exit()
+    # Wait for GPS to stabilise
+    latitude = None
+    i = 0
+    while True:
+      if (gps.data['latitude'] != None) and (str(gps.data['latitude']) != "0.0"):
+        if latitude == gps.data['latitude']:
+          latitude = float(gps.data['latitude'])
+          longitude = float(gps.data['longitude'])
+          elevation = int(round(gps.data['altitude']))
+          break
+      latitude = gps.data['latitude']
+      print(latitude)
+      # Count how many times we've been in this loop
+      i += 1
+      # If it's too many, show a message on screen
+      if i == 10:
+        # Initialise display
+        inky_display, base_img, display_height, display_width, draw = init_display()
+        align_center = display_width // 2
+        message_top = "Waiting for GPS"
+        top_center = align_center - (draw.textlength(message_top, font=astro_constants.font) / 2)
+        draw.text((top_center, 100), message_top, astro_constants.RED, font=astro_constants.font)
+        # Display
+        astro_display(base_img, inky_display)
+      time.sleep(1)
+  else:
+    latitude = float(config['location']['latitude'])
+    longitude = float(config['location']['longitude'])
+    elevation = float(config['location']['elevation'])
+
+  tztext = timezone_at(lng=longitude,lat=latitude)
+  timezone = ZoneInfo(tztext)
+  return(latitude, longitude, elevation, tztext, timezone)
